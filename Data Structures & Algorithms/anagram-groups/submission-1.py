66580ae3b38204946b29e86531f8f9e98a768068
Brendon <145944457+BrendxnW@users.noreply.github.com>
2026-05-28 12:33:55 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        res = defaultdict(list)
+
+        for word in strs:
+            decode = [0] * 26
+
+            for c in word:
+                idx = ord(c) - 97
+                decode[idx] += 1
+            res[tuple(decode)] = res.get(tuple(decode), []) + [word]
+
+        return list(res.values())
